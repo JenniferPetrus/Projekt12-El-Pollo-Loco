eecd5102145c3a2a825c32469aafaee4ae2cc432
Jennifer Petrus <jenny.petrus19@gmail.com>
2024-08-29 18:10:36 +0200

    Design Game

---
+++ models/character.class.js
@@ -0,0 +1,11 @@
+class Character extends MovableObject{
+    height = 250;
+    width = 120;
+    constructor(){
+        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+    }
+
+    jump(){
+
+    }
+}
